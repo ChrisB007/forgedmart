81fd54db42cac6bdafe318748fb6e3fdfb76f656
ChrisB007 <chrisbitoy@gmail.com>
2022-09-02 12:07:15 -0400

    updated mobile view

--- components/Fundform.js
+++ components/Fundform.js
@@ -9,7 +9,7 @@ export default function Funding() {
       >
         <div className="w-full max-w-md space-y-8">
           <div>
-            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
               See if you are eligible for funding
             </h2>
           </div>
