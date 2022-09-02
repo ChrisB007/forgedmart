312fccb4bdb8021f00b88e13fc50416d19d742c4
ChrisB007 <chrisbitoy@gmail.com>
2022-09-02 01:44:08 -0400

    first commit

---
+++ components/Hero.js
@@ -0,0 +1,210 @@
+import { Fragment } from 'react';
+import { Popover, Transition } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import { ChevronRightIcon } from '@heroicons/react/20/solid';
+
+const navigation = [
+  { name: 'Product', href: '#' },
+  { name: 'Features', href: '#' },
+  { name: 'Marketplace', href: '#' },
+  { name: 'Company', href: '#' },
+];
+
+export default function Example() {
+  return (
+    <div className="relative overflow-hidden bg-black">
+      <div
+        className="hidden sm:absolute sm:inset-0 sm:block"
+        aria-hidden="true"
+      >
+        <svg
+          className="absolute bottom-0 right-0 mb-48 translate-x-1/2 transform text-gray-700 lg:top-0 lg:mt-28 lg:mb-0 xl:translate-x-0 xl:transform-none"
+          width={364}
+          height={384}
+          viewBox="0 0 364 384"
+          fill="none"
+        >
+          <defs>
+            <pattern
+              id="eab71dd9-9d7a-47bd-8044-256344ee00d0"
+              x={0}
+              y={0}
+              width={20}
+              height={20}
+              patternUnits="userSpaceOnUse"
+            >
+              <rect x={0} y={0} width={4} height={4} fill="currentColor" />
+            </pattern>
+          </defs>
+          <rect
+            width={364}
+            height={384}
+            fill="url(#eab71dd9-9d7a-47bd-8044-256344ee00d0)"
+          />
+        </svg>
+      </div>
+      <div className="relative pt-6 pb-16 sm:pb-24">
+        <Popover>
+          <nav
+            className="relative mx-auto flex max-w-7xl items-center justify-between px-4 sm:px-6"
+            aria-label="Global"
+          >
+            <div className="flex flex-1 items-center">
+              <div className="flex w-full items-center justify-between md:w-auto">
+                <a href="#">
+                  <span className="sr-only">Workflow</span>
+                  <img
+                    className="h-8 w-auto sm:h-10"
+                    src="/images/Mart.png"
+                    alt=""
+                  />
+                </a>
+                <div className="-mr-2 flex items-center md:hidden">
+                  <Popover.Button className="focus-ring-inset inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-white">
+                    <span className="sr-only">Open main menu</span>
+                    <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+                  </Popover.Button>
+                </div>
+              </div>
+              <div className="hidden space-x-10 md:ml-10 md:flex">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="font-medium text-white hover:text-gray-300"
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+            </div>
+            <div className="hidden md:flex">
+              <a
+                href="#"
+                className="inline-flex items-center rounded-md border border-transparent bg-gray-600 px-4 py-2 text-sm font-medium text-white hover:bg-gray-700"
+              >
+                Partners Login
+              </a>
+            </div>
+          </nav>
+
+          <Transition
+            as={Fragment}
+            enter="duration-150 ease-out"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="duration-100 ease-in"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <Popover.Panel
+              focus
+              className="absolute inset-x-0 top-0 z-10 origin-top-right transform p-2 transition md:hidden"
+            >
+              <div className="overflow-hidden rounded-lg bg-white shadow-md ring-1 ring-black ring-opacity-5">
+                <div className="flex items-center justify-between px-5 pt-4">
+                  <div>
+                    <img
+                      className="h-8 w-auto"
+                      src="https://tailwindui.com/img/logos/workflow-mark.svg?color=red&shade=600"
+                      alt=""
+                    />
+                  </div>
+                  <div className="-mr-2">
+                    <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-red-500">
+                      <span className="sr-only">Close menu</span>
+                      <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                    </Popover.Button>
+                  </div>
+                </div>
+                <div className="space-y-1 px-2 pt-2 pb-3">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-900"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+                <a
+                  href="#"
+                  className="block w-full bg-gray-50 px-5 py-3 text-center font-medium text-red-600 hover:bg-gray-100"
+                >
+                  Partners Login
+                </a>
+              </div>
+            </Popover.Panel>
+          </Transition>
+        </Popover>
+
+        <main className="mt-16 sm:mt-24">
+          <div className="mx-auto max-w-7xl">
+            <div className="lg:grid lg:grid-cols-12 lg:gap-8">
+              <div className="px-4 sm:px-6 sm:text-center md:mx-auto md:max-w-2xl lg:col-span-6 lg:flex lg:items-center lg:text-left">
+                <div>
+                  <a
+                    href="#"
+                    className="inline-flex items-center rounded-full bg-gray-900 p-1 pr-2 text-white hover:text-gray-200 sm:text-base lg:text-sm xl:text-base"
+                  >
+                    <span className="rounded-full bg-red-500 px-3 py-0.5 text-sm font-semibold leading-5 text-white">
+                      Check
+                    </span>
+                    <span className="ml-4 text-sm">Your eligibility</span>
+                    <ChevronRightIcon
+                      className="ml-2 h-5 w-5 text-gray-500"
+                      aria-hidden="true"
+                    />
+                  </a>
+                  <h1 className="mt-4 text-4xl font-bold tracking-tight text-white sm:text-5xl md:text-6xl">
+                    Helping eCommerce companies attain Unicorn status.
+                  </h1>
+                  <p className="mt-3 text-base text-gray-300 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
+                    Not too many businesses built on eCommerce platforms get to
+                    be Billion dollar companies. Question is - Why not?
+                  </p>
+                  <p className="mt-8 text-base font-semibold text-white sm:mt-10">
+                    Serving Companies built on
+                  </p>
+                  <div className="mt-5 w-full sm:mx-auto sm:max-w-lg lg:ml-0">
+                    <div className="flex flex-wrap items-start justify-between">
+                      <div className="flex justify-center px-1">
+                        <img
+                          className="h-9 sm:h-10"
+                          src="/images/shopifyb.png"
+                          alt="Shopify"
+                        />
+                      </div>
+                      <div className="flex justify-center px-1">
+                        <img
+                          className="h-9 sm:h-10"
+                          src="/images/bigc.png"
+                          alt="BigCommerce"
+                        />
+                      </div>
+                      <div className="flex justify-center px-1">
+                        <img
+                          className="h-9 sm:h-10"
+                          src="/images/AWS.png"
+                          alt="amazon"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="mt-16 sm:mt-24 lg:col-span-6 lg:mt-0">
+                <div className="bg-white sm:mx-auto sm:w-full sm:max-w-md sm:overflow-hidden sm:rounded-lg">
+                  <div>
+                    <img src="/images/storeowners.jpg" alt="" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
---
+++ components/Services.js
@@ -0,0 +1,292 @@
+import {
+  BoltIcon,
+  ChatBubbleBottomCenterTextIcon,
+  EnvelopeIcon,
+  GlobeAltIcon,
+  ScaleIcon,
+} from '@heroicons/react/24/outline';
+import Funding from '../components/Fundform';
+
+const transferFeatures = [
+  {
+    id: 1,
+    name: 'Funding & Growth',
+    description:
+      'In order to facilitate growth you need capital. This is optional but recommended if you are planning to scale your business and running low on funds or not enough capital after expenses are taken care of. We can provide funding and resources for you to grow faster.',
+    icon: GlobeAltIcon,
+  },
+  {
+    id: 2,
+    name: 'Branding & Marketing',
+    description:
+      'Selling your own products with your company name and logo is a key factor when it comes to branding. If you are not currently selling your own brand product we work with an array of product designers and manufacturers that can help bring your ideas to live as well as a winning go-to-market strategy.',
+    icon: ScaleIcon,
+  },
+  {
+    id: 3,
+    name: 'Exit Strategy',
+    description:
+      'At this point your company has a track record of successfully operating over some time, and you are now ready to reap the benefits of your hard work either to be acquired by a bigger company or make offerings to go public',
+    icon: BoltIcon,
+  },
+];
+const communicationFeatures = [
+  {
+    id: 1,
+    name: 'Dedicated Team',
+    description:
+      'Your company with be assigned an Account Manager to oversee your growth along with dedicated experts in marketing and branding. Your Account Manager will put together a road map with an executable plan to succeed',
+    icon: ChatBubbleBottomCenterTextIcon,
+  },
+  {
+    id: 2,
+    name: 'Access to Funding',
+    description:
+      'Let\'s start by saying that this is not a loan. However it is a "recommended" revenue-based financing that you can choose (if needed) to access. Your company will have revolving access to funds for you to grow faster afterall the faster you grows, the sooner you attain unicorn status',
+    icon: EnvelopeIcon,
+  },
+  {
+    id: 2,
+    name: 'Dedicated Branding & Marketing team',
+    description:
+      'Our team will propose branding ideas, help improve your marketing performance on the web as well as parallel your social media effort so that you can acquire more customers and increase profitability.',
+    icon: EnvelopeIcon,
+  },
+  {
+    id: 2,
+    name: 'Exit Strategy Advisor',
+    description:
+      'As exciting it sounds to run your company forever, most entrepreneurs plan on retiring at some point and exiting the company. You need an expert to advise you on when is right to exit, how exit strategy help your business valuation, types of exit strategies, which strategy is best for your company e.t.c.  ',
+    icon: EnvelopeIcon,
+  },
+];
+
+export default function Services() {
+  return (
+    <div className="overflow-hidden bg-gray-50 py-16 lg:py-24">
+      <div className="relative mx-auto max-w-xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+        <svg
+          className="absolute left-full hidden -translate-x-1/2 -translate-y-1/4 transform lg:block"
+          width={404}
+          height={784}
+          fill="none"
+          viewBox="0 0 404 784"
+          aria-hidden="true"
+        >
+          <defs>
+            <pattern
+              id="b1e6e422-73f8-40a6-b5d9-c8586e37e0e7"
+              x={0}
+              y={0}
+              width={20}
+              height={20}
+              patternUnits="userSpaceOnUse"
+            >
+              <rect
+                x={0}
+                y={0}
+                width={4}
+                height={4}
+                className="text-gray-200"
+                fill="currentColor"
+              />
+            </pattern>
+          </defs>
+          <rect
+            width={404}
+            height={784}
+            fill="url(#b1e6e422-73f8-40a6-b5d9-c8586e37e0e7)"
+          />
+        </svg>
+
+        <div className="relative">
+          <h2 className="text-center text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">
+            Providing robust strategies you need to Scale and Exit
+          </h2>
+          <p className="mx-auto mt-4 max-w-3xl text-center text-xl text-gray-500">
+            From growth funding to brand building strategies, brand marketing to
+            investors relations strategies, we provide practical value that
+            accelerate growth for eCommerce businesses.
+          </p>
+        </div>
+
+        <div className="relative mt-12 lg:mt-24 lg:grid lg:grid-cols-2 lg:items-center lg:gap-8">
+          <div className="relative">
+            <h3 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
+              How it works
+            </h3>
+            <p className="mt-3 text-lg text-gray-500">
+              The way we work with every company is tailored to their specific
+              needs. However, it falls within these categories
+            </p>
+
+            <dl className="mt-10 space-y-10">
+              {transferFeatures.map((item) => (
+                <div key={item.id} className="relative">
+                  <dt>
+                    <div className="absolute flex h-12 w-12 items-center justify-center rounded-md bg-red-500 text-white">
+                      <item.icon className="h-6 w-6" aria-hidden="true" />
+                    </div>
+                    <p className="ml-16 text-lg font-medium leading-6 text-gray-900">
+                      {item.name}
+                    </p>
+                  </dt>
+                  <dd className="mt-2 ml-16 text-base text-gray-500">
+                    {item.description}
+                  </dd>
+                </div>
+              ))}
+            </dl>
+          </div>
+
+          <div className="relative -mx-4 mt-10 lg:mt-0" aria-hidden="true">
+            <svg
+              className="absolute left-1/2 -translate-x-1/2 translate-y-16 transform lg:hidden"
+              width={784}
+              height={404}
+              fill="none"
+              viewBox="0 0 784 404"
+            >
+              <defs>
+                <pattern
+                  id="ca9667ae-9f92-4be7-abcb-9e3d727f2941"
+                  x={0}
+                  y={0}
+                  width={20}
+                  height={20}
+                  patternUnits="userSpaceOnUse"
+                >
+                  <rect
+                    x={0}
+                    y={0}
+                    width={4}
+                    height={4}
+                    className="text-gray-200"
+                    fill="currentColor"
+                  />
+                </pattern>
+              </defs>
+              <rect
+                width={784}
+                height={404}
+                fill="url(#ca9667ae-9f92-4be7-abcb-9e3d727f2941)"
+              />
+            </svg>
+
+            <div className="relative mx-auto home-form">
+              <Funding />
+            </div>
+          </div>
+        </div>
+
+        <svg
+          className="absolute right-full hidden translate-x-1/2 translate-y-12 transform lg:block"
+          width={404}
+          height={784}
+          fill="none"
+          viewBox="0 0 404 784"
+          aria-hidden="true"
+        >
+          <defs>
+            <pattern
+              id="64e643ad-2176-4f86-b3d7-f2c5da3b6a6d"
+              x={0}
+              y={0}
+              width={20}
+              height={20}
+              patternUnits="userSpaceOnUse"
+            >
+              <rect
+                x={0}
+                y={0}
+                width={4}
+                height={4}
+                className="text-gray-200"
+                fill="currentColor"
+              />
+            </pattern>
+          </defs>
+          <rect
+            width={404}
+            height={784}
+            fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)"
+          />
+        </svg>
+
+        <div className="relative mt-12 sm:mt-16 lg:mt-24">
+          <div className="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:items-center lg:gap-8">
+            <div className="lg:col-start-2">
+              <h3 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
+                What to Expect
+              </h3>
+              <p className="mt-3 text-lg text-gray-500">
+                Below are some of the services you are guarantee to expect from
+                us.
+              </p>
+
+              <dl className="mt-10 space-y-10">
+                {communicationFeatures.map((item) => (
+                  <div key={item.id} className="relative">
+                    <dt>
+                      <div className="absolute flex h-12 w-12 items-center justify-center rounded-md bg-red-500 text-white">
+                        <item.icon className="h-6 w-6" aria-hidden="true" />
+                      </div>
+                      <p className="ml-16 text-lg font-medium leading-6 text-gray-900">
+                        {item.name}
+                      </p>
+                    </dt>
+                    <dd className="mt-2 ml-16 text-base text-gray-500">
+                      {item.description}
+                    </dd>
+                  </div>
+                ))}
+              </dl>
+            </div>
+
+            <div className="relative -mx-4 mt-10 lg:col-start-1 lg:mt-0">
+              <svg
+                className="absolute left-1/2 -translate-x-1/2 translate-y-16 transform lg:hidden"
+                width={784}
+                height={404}
+                fill="none"
+                viewBox="0 0 784 404"
+                aria-hidden="true"
+              >
+                <defs>
+                  <pattern
+                    id="e80155a9-dfde-425a-b5ea-1f6fadd20131"
+                    x={0}
+                    y={0}
+                    width={20}
+                    height={20}
+                    patternUnits="userSpaceOnUse"
+                  >
+                    <rect
+                      x={0}
+                      y={0}
+                      width={4}
+                      height={4}
+                      className="text-gray-200"
+                      fill="currentColor"
+                    />
+                  </pattern>
+                </defs>
+                <rect
+                  width={784}
+                  height={404}
+                  fill="url(#e80155a9-dfde-425a-b5ea-1f6fadd20131)"
+                />
+              </svg>
+              <img
+                className="relative mx-auto rounded-md"
+                width={490}
+                src="/images/exit.jpg"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
