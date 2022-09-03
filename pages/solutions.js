6926f77b4857c05cd59cf5511e058139e6cddd4f
ChrisB007 <chrisbitoy@gmail.com>
2022-09-03 14:26:56 -0400

    updated solutions page

--- pages/solutions.js
+++ pages/solutions.js
@@ -24,11 +24,8 @@ export default function Example() {
         <div className="absolute top-0 bottom-0 left-3/4 hidden w-screen bg-gray-50 lg:block" />
         <div className="mx-auto max-w-prose text-base lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-8">
           <div>
-            <h2 className="text-lg font-semibold text-indigo-600">
-              Case Study
-            </h2>
             <h3 className="mt-2 text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">
-              Meet Whitney
+              Our Solutions
             </h3>
           </div>
         </div>
@@ -84,42 +81,39 @@ export default function Example() {
           <div className="mt-8 lg:mt-0">
             <div className="mx-auto max-w-prose text-base lg:max-w-none">
               <p className="text-lg text-gray-500">
-                Sagittis scelerisque nulla cursus in enim consectetur quam.
-                Dictum urna sed consectetur neque tristique pellentesque.
-                Blandit amet, sed aenean erat arcu morbi.
+                We invest various resources in helping eCommerce businesses
+                scale and increase ROI through funding, brand marketing, and
+                eventually providing an exit strategy.
               </p>
             </div>
             <div className="prose prose-indigo mx-auto mt-5 text-gray-500 lg:col-start-1 lg:row-start-1 lg:max-w-none">
-              <p>
-                Sollicitudin tristique eros erat odio sed vitae, consequat
-                turpis elementum. Lorem nibh vel, eget pretium arcu vitae. Eros
-                eu viverra donec ut volutpat donec laoreet quam urna.
-              </p>
-              <p>
-                Bibendum eu nulla feugiat justo, elit adipiscing. Ut tristique
-                sit nisi lorem pulvinar. Urna, laoreet fusce nibh leo. Dictum et
-                et et sit. Faucibus sed non gravida lectus dignissim imperdiet
-                a.
-              </p>
-              <p>
-                Dictum magnis risus phasellus vitae quam morbi. Quis lorem lorem
-                arcu, metus, egestas netus cursus. In.
-              </p>
-              <ul role="list">
-                <li>Quis elit egestas venenatis mattis dignissim.</li>
-                <li>
-                  Cras cras lobortis vitae vivamus ultricies facilisis tempus.
-                </li>
-                <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
-              </ul>
-              <p>
-                Rhoncus nisl, libero egestas diam fermentum dui. At quis
-                tincidunt vel ultricies. Vulputate aliquet velit faucibus
-                semper. Pellentesque in venenatis vestibulum consectetur nibh
-                id. In id ut tempus egestas. Enim sit aliquam nec, a. Morbi enim
-                fermentum lacus in. Viverra.
-              </p>
-              <h3>How we helped</h3>
+              <div className="mb-3">
+                <h2 className="text-lg font-bold ">Capital</h2>
+                <p>
+                  Bibendum eu nulla feugiat justo, elit adipiscing. Ut tristique
+                  sit nisi lorem pulvinar. Urna, laoreet fusce nibh leo. Dictum
+                  et et et sit. Faucibus sed non gravida lectus dignissim
+                  imperdiet a.
+                </p>
+                <ul role="list">
+                  <li>Quis elit egestas venenatis mattis dignissim.</li>
+                  <li>
+                    Cras cras lobortis vitae vivamus ultricies facilisis tempus.
+                  </li>
+                  <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
+                </ul>
+              </div>
+              <div className="mb-3">
+                <h2 className="text-lg font-bold ">Brand Marketing</h2>
+                <p>
+                  Rhoncus nisl, libero egestas diam fermentum dui. At quis
+                  tincidunt vel ultricies. Vulputate aliquet velit faucibus
+                  semper. Pellentesque in venenatis vestibulum consectetur nibh
+                  id. In id ut tempus egestas. Enim sit aliquam nec, a. Morbi
+                  enim fermentum lacus in. Viverra.
+                </p>
+              </div>
+              <h3 className="text-lg font-bold ">Exit Strategy</h3>
               <p>
                 Tincidunt integer commodo, cursus etiam aliquam neque, et.
                 Consectetur pretium in volutpat, diam. Montes, magna cursus
