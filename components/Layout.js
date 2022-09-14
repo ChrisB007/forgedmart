372404e0cf924c2471e2ea89c047706269051e72
ChrisB007 <chrisbitoy@gmail.com>
2022-09-14 15:37:10 -0400

    updated head section

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Nav from './Nav';
 import Footer from './Footer';
+import Head from 'next/head';
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Head>
+        <title>ForgedMart</title>
+        <meta
+          name="description"
+          content="Helping eCommerce businesses attain Unicorn status. From growth funding to brand building strategies, brand marketing to investor relations strategies..."
+        />
+      </Head>
       <Nav />
       {children}
       <Footer />
