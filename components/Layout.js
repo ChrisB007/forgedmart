0264d415c1add940b1f28678320a652e34a2755c
ChrisB007 <chrisbitoy@gmail.com>
2022-09-14 16:54:43 -0400

    added keywords

--- components/Layout.js
+++ components/Layout.js
@@ -12,6 +12,10 @@ const Layout = ({ children }) => {
           name="description"
           content="ForgedMart - Helping eCommerce businesses attain Unicorn status. From growth funding to brand building strategies, brand marketing to investor relations strategies..."
         />
+        <meta
+          name="keywords"
+          content="forgedmart, shopify, shopify stores, amazon fba program, shopify partners, funding,amazon fba business, business loans, shopify website, shopifyplus, amazon seller, amazon fba, big commerce, ecommerce"
+        />
       </Head>
       <Nav />
       {children}
