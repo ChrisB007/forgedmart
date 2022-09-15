e41b6c9cfdef2352a05c21892931e6deb97f7281
ChrisB007 <chrisbitoy@gmail.com>
2022-09-15 14:06:05 -0400

    fundform updated

--- components/Fundform.js
+++ components/Fundform.js
@@ -159,7 +159,7 @@ export default function Funding() {
                   autoComplete="duration"
                   required
                   className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm"
-                  placeholder="Months in numbers (Example: 3)"
+                  placeholder="Number of months (Example: 3)"
                   onChange={handleChange}
                 />
               </div>
